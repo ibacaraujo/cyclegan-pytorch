377678f9df7e55a9acd9a27c652f307db06b9e59
Iago Breno <ibacaraujo@gmail.com>
2020-04-01 23:24:57 -0300

    Add parse function

--- options/base_options.py
+++ options/base_options.py
@@ -8,7 +8,7 @@ import data
 class BaseOptions():
     
     def __init__(self):
-        self.initialized = self
+        self.initialized = False
 
     def initialize(self, parser):
         # basic parameters
@@ -88,4 +88,23 @@ class BaseOptions():
             opt_file.write('\n')
 
     def parse(self):
-        pass
+        opt = self.gather_options()
+        opt.isTrain = self.isTrain
+
+        if opt.suffix:
+            suffix = (' ' + opt.suffix.format(**vars(opt))) if opt.suffix != '' else ''
+            opt.name = opt.name + suffix
+
+        self.print_options(opt)
+
+        str_ids = opt.gpu_ids.split(',')
+        opt.gpu_ids = []
+        for str_id in str_ids:
+            id = int(str_id)
+            if id >= 0:
+                opt.gpu_ids.append(id)
+        if len(opt.gpu_ids) > 0:
+            torch.cuda.set_device(opt.gpu_ids[0])
+        
+        self.opt = opt
+        return self.opt
