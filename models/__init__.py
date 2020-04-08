12c2792d6b67bea61bf7dbfaf1dd952c6015c7f9
Iago Breno <ibacaraujo@gmail.com>
2020-04-08 01:59:36 -0300

    Add find_model_using_name function

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,20 @@ import importlib
 from models.base_model import BaseModel
 
 def find_model_using_name(model_name):
-    pass
+    model_filename = "model." + model_name + "_model"
+    modellib = importlib.import_module(model_filename)
+    model = None
+    target_model_name = model_name.replace('_', '') + 'model'
+    for name, cls in modellib.__dict__.items():
+        if name.lower() == target_model_name.lower() \
+            and issubclass(cls, BaseModel):
+              model = cls
+
+    if model is None:
+        print("In %s.py, there should be a subclass of BaseModel with class name that matches %s in lowercase." % (model_filename, target_model_name))
+        exit(0)
+
+    return model
 
 def get_option_setter(model_name):
     pass
